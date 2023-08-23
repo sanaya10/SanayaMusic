e0417d25da1f399ced0de1c74d85ed54337278bb
Romaan313 <108215028+Romaan313@users.noreply.github.com>
2023-08-23 18:27:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "˹𝙉𝘼𝙔𝙍𝘼˼. ᴍᴜsɪᴄ")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1985742354").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
